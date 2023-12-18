8c30b27748d1f39539dae221abd706f0d16d0ea0
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-12-18 23:36:59 +0800

    Using bootstrap framework for CSS

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,52 +1,54 @@
 <template>
   <div id="app">
-    <div>
-      <h1>Заметки</h1>
+    <div  class="card p-5">
+      <h1>Новая заметка</h1>
 
       <form>
-        <div>
-          <label for="text">Задача </label>
-          <input type="text" id="text" v-model="text">
-        </div>
-        <div>
-	  <label for="type">Тип </label>
-		<select id="type" v-model="type">
-  		<option value="Личное">Личное</option>
-  		<option value="Мероприятие">Мероприятие</option>
-  		<option value="Работа">Работа</option>
-  		<option value="Другое">Другое</option>
-	  </select>
-
-	</div>
-
-        <div>
-          <label>Срочность:</label>
-          <div>
-            <input type="radio" id="priority-low" value="Не срочно" v-model="priority">
-            <label for="priority-low">Не срочно</label>
-          </div>
-          <div>
-            <input type="radio" id="priority-high" value="Срочно" v-model="priority">
-            <label for="priority-high">Срочно</label>
-          </div>
-        </div>
-
-        <button @click="add">Добавить заметку</button>
-        <button @click="reset">Сбросить данные</button>
+		<div class="row">
+        		<div class="col mb-3">
+          			<label for="text" class="form-label">Задача </label>
+          			<input type="text" id="text" v-model="text" class="form-control">
+        		</div>
+        		<div class="col mb-3">
+	  			<label for="type" class="form-label">Тип </label>
+				<select id="type" v-model="type"  class="form-control">
+  					<option value="Личное">Личное</option>
+  					<option value="Мероприятие">Мероприятие</option>
+  					<option value="Работа">Работа</option>
+  					<option value="Другое">Другое</option>
+	  			</select>
+			</div>
+		</div>
+        	<div>
+			<label class="form-label">Срочность:</label>
+			<div class="form-check form-check-inline">
+            			<input type="radio" id="priority-low" value="Не срочно" v-model="priority"  class="form-check-input" >
+            			<label for="priority-low" class="form-label">Не срочно</label>
+			</div>
+			<div  class="form-check form-check-inline">
+				<input type="radio" id="priority-high" value="Срочно" v-model="priority"  class="form-check-input" >
+				<label for="priority-high" class="form-label">Срочно</label>
+			</div>
+        	</div>
+		<div class="row">
+        		<div class="col mb-3">
+        			<button @click="add" class="btn btn-primary">Добавить заметку</button>
+        		</div>
+        		<div class="col mb-3">
+        			<button @click="reset" class="btn btn-primary">Сбросить данные</button>
+  			</div>
+  		</div>
       </form>
-
-<ul>
-        <li v-for="note in this.notes">
-          <div>
-            <p><b>{{ note.text }} </b>: {{ note.type }} ({{ note.priority }}) {{ note.check ? '✔' : ' ' }}<input type="checkbox" v-model="note.check" id="check"></p>
-            <div>
-              
-            </div>
-          </div>
-        </li>
+</div>
+<h1>Мои заметки</h1>
+<div class="card p-5">
+	<ul>
+        	<li v-for="note in this.notes">
+          		<div>
+            			<p><b>{{ note.text }} </b>: {{ note.type }} ({{ note.priority }}) {{ note.check ? '✔' : ' ' }}<input type="checkbox" v-model="note.check" id="check"  class="form-check-input" ></p>
+          		</div>
+        	</li>
       </ul>
-
-
     </div>
   </div>
 </template>
@@ -96,13 +98,12 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
 }
 ul {
   list-style: none;
 }
 div, button {
-  margin: 10px;
+margin: 5px;
 }
 </style>
